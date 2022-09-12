eef8cf7546537bbcb9a7161d4e170c0f1ab3fbc1
magesh100 <mmageshofficial@gmail.com>
2022-09-12 14:03:18 +0530

    edited
    edited

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-alert('hi');
-const choices =["rock","paper","scissors"];
+function startGame(){
+let array = ["paper","scissor","rock"];
+cc = array[Math.floor(Math.random()*array.length)];
+input = prompt("Enter Your Choice");
+uc = input;
+if(cc == "rock" && uc=="paper"){
+  alert("You Won");
+}
+else if(cc == "rock" &&  uc == "scissor") {
+  alert("computer won");
+}
+else if( cc  == "paper"  &&  uc =="rock") {
+  alert("computer won"); 
+}
+else if( cc =="paper" &&  uc == "scissor") {
+  alert("You won");
+}
+else if( cc == "scissor" && uc =="rock"){
+  alert("you won");
+}
+else if( cc = "scissor"  &&  uc =="paper"){
+  alert("computer won");
+}
+else{
+  alert("tie");
+}
+
+
+
+}
