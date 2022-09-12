f0db9df524b95794675f13afaed8e2a60816a4a8
magesh100 <mmageshofficial@gmail.com>
2022-09-12 17:45:59 +0530

    edit
    edit

--- script.js
+++ script.js
@@ -3,39 +3,37 @@ let array = ["paper","scissor","rock"];
 cc = array[Math.floor(Math.random()*array.length)];
 input = prompt("Enter Your Choice");
 uc = input;
-if(cc == "rock" && uc=="paper"){
+if(cc =="rock" && uc=="paper"){
   alert("You Won");
 }
-else if(cc == "rock" &&  uc == "scissor") {
+else if (cc == "paper" &&  uc == "paper") {
+  alert("tie");
+}
+else if (cc == "rock" &&  uc == "rock") {
+  alert("tie");
+}
+else if (cc == "scissor" &&  uc == "scissor") {
+  alert("tie");
+}
+else if (cc =="rock" &&  uc =="scissor") {
   alert("computer won");
 }
-else if( cc  == "paper"  &&  uc =="rock") {
+else if ( cc  =="paper"  &&  uc =="rock") {
   alert("computer won"); 
 }
-else if( cc =="paper" &&  uc == "scissor") {
+else if ( cc == "paper" &&  uc == "scissor") {
   alert("You won");
 }
-else if( cc == "scissor" && uc =="rock"){
+else if ( cc == "scissor" && uc == "rock"){
   alert("you won");
 }
-else if( cc = "scissor"  &&  uc =="paper"){
+else if ( cc = "scissor"  &&  uc == "paper") {
   alert("computer won");
 }
-else if( cc == "paper" && uc =="paper"){
-  alert("it's tie");
+
+else {
+alert("Please provide a valid input!");
 }
-else if( cc == "rock" && uc =="rock"){
-  alert("it's tie");
-}
-else if( cc == "scissor" && uc =="scissor"){
-  alert("it's tie");
 }
 
 
-else{
-  alert("Please provide a valid input!");
-}
-
-
-
-}
